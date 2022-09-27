d86a4cfda2ae65ddb87468d0347fef1ae0a89d8f
khaihuyennguyen <nguyenkhaihuyen1990@gmail.com>
2022-09-26 22:59:06 -0500

    add

--- additiveNumber.py
+++ additiveNumber.py
@@ -1,10 +1,11 @@
+import itertools 
 class Solution(object):
     def isAddictiveNumber(self, num: str) -> bool:
-        res = []
-        if len(num) <= 2:
-            return False
-        
-        def get_all_nums(first, second, index):
-            s = str(first) + str(second)
-            while len(s) < len(num):
-                
+        def valid(a, b, s):
+            if a != str(int(a)) or b != str(int(b)): return   
+            if not s: return True
+            c = str(int(a) + int(b))
+            return s.startswith(c) and valid(b,c,s[len(c):])
+        return any(valid(num[:i], num[i:j], num[j:]) for i,j in itertools.combinations(range(1, len(num)),2))
+
+print(Solution().isAddictiveNumber("112358"))
